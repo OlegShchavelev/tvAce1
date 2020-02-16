55000c910410b7fedb3bdf09cb8117fd0f2a6ecc
DetaliDigital <admin@detali.digital>
2020-02-16 19:22:46 +0300

    Фикс

--- core/components/tvace/elements/plugins/plugin.tvace.php
+++ core/components/tvace/elements/plugins/plugin.tvace.php
@@ -4,7 +4,7 @@ $corePath = $modx->getOption('core_path',null,MODX_CORE_PATH).'components/tvace/
 
 switch ($modx->event->name) {
     case 'OnTVInputRenderList':
-        $modx->lexicon->load('tvace:tvs');
+        $modx->lexicon->load('tvace:tv');
         $modx->event->output($corePath.'tv/input/');
         break;
     case 'OnManagerPageBeforeRender':
