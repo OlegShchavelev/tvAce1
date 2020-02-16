da818aa18fa9f163895beea10a4d3688318c89f3
DetaliDigital <admin@detali.digital>
2020-02-16 19:28:56 +0300

    Поправил структуру.

--- core/components/tvace/elements/plugins/plugin.tvace.php
+++ core/components/tvace/elements/plugins/plugin.tvace.php
@@ -5,7 +5,7 @@ $corePath = $modx->getOption('core_path',null,MODX_CORE_PATH).'components/tvace/
 switch ($modx->event->name) {
     case 'OnTVInputRenderList':
         $modx->lexicon->load('tvace:tv');
-        $modx->event->output($corePath.'tv/input/');
+        $modx->event->output($corePath.'elements/tv/input/');
         break;
     case 'OnManagerPageBeforeRender':
         break;
